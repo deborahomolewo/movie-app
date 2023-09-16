f6a065267451e4058c5e1e289411982abcdc724a
Deborah <omolewod@gmail.com>
2023-09-16 18:55:51 +0100

    Update Movie.jsx

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -8,7 +8,7 @@ function Movie({movie}) {
                 <img data-testid= "movie-poster" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="poster" />
             </div>
             <div className="content">
-                <h1 data-testid="movie-title">{movie.original_title}</h1>
+                <h1 data-testid="movie-title">{movie.title}</h1>
                 <p data-testid="movie-release-date">{movie.release_date}</p>
             </div>
         </div>
